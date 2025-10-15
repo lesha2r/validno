f5ca6d6dbbd52b1eccf2916f3d2c5ceeb8f0cef2
lesha2r <leshatour@gmail.com>
2025-10-15 20:19:23 +0300

    fix: add named export for ValidnoResult in index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,10 @@
 import { Schema } from "./Schema.js";
 import _validations from "./utils/validations.js";
+import ValidnoResult from "./engine/ValidnoResult.js";
 
+// Extras. Named exports
 export const validations = _validations
+export { ValidnoResult }
+
+// Default
 export default Schema
